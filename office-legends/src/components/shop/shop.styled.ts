8dd7c342ca699674b9f8db2238f432769633469d
Kamil <k.wypych@pap.pl>
2023-05-26 20:53:02 +0200

    refactor: modified shop styles

--- office-legends/src/components/shop/shop.styled.ts
+++ office-legends/src/components/shop/shop.styled.ts
@@ -4,6 +4,9 @@ import { variables } from 'variables';
 import shop from '../../assets/images/shop.png';
 
 export const Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: ${variables.SHOP_LEFT_POS}px;
   background-image: url(${shop});
   width: ${variables.SHOP_WIDTH}px;
   height: ${variables.SHOP_HEIGHT}px;
