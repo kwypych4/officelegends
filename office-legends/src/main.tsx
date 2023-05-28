677ee1b2098b02d8fabb79551e0dd358a5f96136
Kamil <k.wypych@pap.pl>
2023-05-28 17:43:44 +0200

    refactor: implemented RQ Client

--- office-legends/src/main.tsx
+++ office-legends/src/main.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { RouterProvider } from 'react-router-dom';
 import { Router } from 'router';
 import { GlobalStyles } from 'styles';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 20000,
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
   // TODO: add dependency if dev
-  <>
+  <QueryClientProvider client={queryClient}>
     <GlobalStyles />
     <RouterProvider router={Router} />
-  </>
+  </QueryClientProvider>
 
   // </React.StrictMode>
 );
