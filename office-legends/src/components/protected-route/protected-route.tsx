0102065bb7685773fbb8e7801fcf1f331d609649
Kamil <k.wypych@pap.pl>
2023-05-29 21:10:28 +0200

    refactor: modified protected route and added protection from auth layout

--- office-legends/src/components/protected-route/protected-route.tsx
+++ office-legends/src/components/protected-route/protected-route.tsx
@@ -1,18 +1,28 @@
 import { api } from 'api';
 import { useCustomQuery } from 'hooks';
 import { Navigate } from 'react-router-dom';
-import { useAuthStore } from 'store';
+import { useAuthStore, useUserStore } from 'store';
 import { appRoutes } from 'urls';
 
-export const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+export const ProtectedRoute = ({ children, isAuthLayout }: { children: JSX.Element; isAuthLayout?: boolean }) => {
   const isLogged = useAuthStore((state) => state.isLogged);
 
   const isLoggedQuery = useCustomQuery(['verify'], () => api.auth.verify(), {
-    onSuccess: () => {
+    onSuccess: ({ avatar, exp, gameServer, id, money, skin, username }) => {
       useAuthStore.setState({ isLogged: true });
+      useUserStore.setState({ avatar, exp, gameServer, id, money, skin, username });
     },
     onError: () => {
       useAuthStore.setState({ isLogged: false });
+      useUserStore.setState({
+        avatar: null,
+        exp: null,
+        gameServer: null,
+        id: null,
+        money: null,
+        skin: null,
+        username: null,
+      });
     },
     staleTime: Infinity,
     retry: false,
@@ -20,6 +30,8 @@ export const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   });
 
   if (isLoggedQuery.isLoading) return <div>loading</div>;
+
+  if (isAuthLayout) return isLogged ? <Navigate to={appRoutes.app.game} /> : children;
 
   return isLogged ? children : <Navigate to={appRoutes.auth.login} />;
 };
