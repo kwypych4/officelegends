50d11381d2a294d610bc3675d481f1fd07327409
Kamil <k.wypych@pap.pl>
2023-05-29 21:09:38 +0200

    refactor: added user status type

---
+++ office-legends/src/types/user.types.ts
@@ -0,0 +1,9 @@
+export type UserStatus = {
+  id: number | null;
+  money: number | null;
+  exp: number | null;
+  username: string | null;
+  skin: number | null;
+  avatar: number | null;
+  gameServer: number | null;
+};
