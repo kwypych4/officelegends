133228e1f7bec48eafb873fd77d80617bfec42b5
Kamil <k.wypych@pap.pl>
2023-05-30 23:06:37 +0200

    refactor: modified styles

--- office-legends/src/pages/game/game.styled.ts
+++ office-legends/src/pages/game/game.styled.ts
@@ -3,9 +3,10 @@ import { variables } from 'variables';
 
 export const GameWrapper = styled.div`
   display: flex;
-
+  position: relative;
   width: ${variables.BOARD_WIDTH}px;
   min-width: ${variables.BOARD_WIDTH}px;
   height: ${variables.BOARD_HEIGHT}px;
   min-height: ${variables.BOARD_HEIGHT}px;
+  overflow: hidden;
 `;
