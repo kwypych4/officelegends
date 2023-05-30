57542269ee0e90b4a39558d26e47a69517541ef9
Kamil <k.wypych@pap.pl>
2023-05-30 23:07:15 +0200

    feat: game store can store more states

--- office-legends/src/store/game.ts
+++ office-legends/src/store/game.ts
@@ -2,8 +2,12 @@ import { create } from 'zustand';
 
 type GameStateType = {
   isHallOfFameDoorOpen: boolean;
+  isShopOpened: boolean;
+  isPlayroomOpened: boolean;
 };
 
 export const useGameStore = create<GameStateType>(() => ({
   isHallOfFameDoorOpen: false,
+  isShopOpened: false,
+  isPlayroomOpened: false,
 }));
