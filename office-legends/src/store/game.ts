dd12a7af5fda939fba2ca36f6578024698d73da3
Kamil <k.wypych@pap.pl>
2023-06-01 21:27:07 +0200

    feat: implemented websocket global

--- office-legends/src/store/game.ts
+++ office-legends/src/store/game.ts
@@ -1,13 +1,22 @@
+import io, { Socket } from 'socket.io-client';
+import { ClientToServerEvents, PlayerProperties, ServerToClientEvents } from 'types';
 import { create } from 'zustand';
 
 type GameStateType = {
   isHallOfFameDoorOpen: boolean;
   isShopOpened: boolean;
   isPlayroomOpened: boolean;
+  socket: Socket<ServerToClientEvents, ClientToServerEvents>;
+  playersList: PlayerProperties[] | null;
 };
 
 export const useGameStore = create<GameStateType>(() => ({
   isHallOfFameDoorOpen: false,
   isShopOpened: false,
   isPlayroomOpened: false,
+  socket: io('localhost:3000', {
+    transports: ['websocket'],
+    autoConnect: true,
+  }),
+  playersList: null,
 }));
--- office-legends/src/types/websocket.types.ts
+++ office-legends/src/types/websocket.types.ts
@@ -1,19 +1,30 @@
 import { DirectionsType } from './directions.types';
+import { PlayerProperties } from './player.types';
 
-export type WebsocketRequestType = {
+export type MoveEmitType = {
   direction: DirectionsType;
   position: {
     x: number;
     y: number;
   };
 };
-export type WebsocketResponseType = {
-  username: string;
-  id: number;
+export type MoveResponseType = {
+  player: number;
+  direction: DirectionsType;
   position: {
     x: number;
     y: number;
   };
-  action: Array<DirectionsType | null>;
-  image: number;
-}[];
+};
+
+export type ServerToClientEvents = {
+  join: ({ playersList }: { playersList: PlayerProperties[] }) => void;
+  leave: ({ playersList }: { playersList: PlayerProperties[] }) => void;
+  move: (response: MoveResponseType) => void;
+};
+
+export type ClientToServerEvents = {
+  join: ({ gameServer }: { gameServer: number | null }) => void;
+  leave: () => void;
+  move: (move: MoveEmitType) => void;
+};
