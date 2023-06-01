ed71278eebe6561b96fb35d8f40531e6bae6f4fb
Kamil <k.wypych@pap.pl>
2023-06-01 21:29:05 +0200

    feat: game connects to server

--- office-legends/src/pages/game/game.tsx
+++ office-legends/src/pages/game/game.tsx
@@ -1,4 +1,5 @@
-import { useUserStore } from 'store';
+import { useEffect } from 'react';
+import { useGameStore, useUserStore } from 'store';
 
 import { GameChoose, PlaygroundMap } from '.';
 import { HallOfFameMap, Settings } from './components';
@@ -6,9 +7,30 @@ import { GameWrapper } from './game.styled';
 
 export const GamePage = () => {
   const { gameServer } = useUserStore();
+  const { socket } = useGameStore();
+
+  useEffect(() => {
+    if (gameServer !== undefined) {
+      socket.emit('join', {
+        gameServer,
+      });
+    }
+
+    return () => {
+      socket.emit('leave');
+    };
+  }, [gameServer, socket]);
+
+  socket.on('join', ({ playersList }) => {
+    useGameStore.setState({ playersList });
+  });
+  socket.on('leave', ({ playersList }) => {
+    useGameStore.setState({ playersList });
+  });
 
   const getGame = () => {
     if (gameServer === null || gameServer === undefined) return <GameChoose />;
+    if (!socket.connected) return <div>Loading...</div>;
     if (gameServer === 2) return <HallOfFameMap />;
 
     return <PlaygroundMap />;
