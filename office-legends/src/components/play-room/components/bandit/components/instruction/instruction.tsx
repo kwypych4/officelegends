bc56e90d972fd942cf1ff0b4012200556bfa2972
Kamil <k.wypych@pap.pl>
2023-06-04 21:02:59 +0200

    feat: added component with instruction

---
+++ office-legends/src/components/play-room/components/bandit/components/instruction/instruction.tsx
@@ -0,0 +1,13 @@
+export const Instruction = () => {
+  return (
+    <div>
+      <h4>Instruction:</h4>
+      <p>To play you have to have credits. You can buy them here - 5 credits costs 10$. If you will earn:</p>
+      <ul>
+        <li>15$ if you will spin 3 cherries</li>
+        <li>30$ if you will spin 3 diamonds</li>
+        <li>50$ if you will spin 3 crowns</li>
+      </ul>
+    </div>
+  );
+};
