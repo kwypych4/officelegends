614993d8acb7bacb76debc12a70695bceace888e
Kamil <k.wypych@pap.pl>
2023-06-07 01:42:46 +0200

    refactor: modified exports

--- office-legends/src/components/index.ts
+++ office-legends/src/components/index.ts
@@ -1,6 +1,6 @@
 export * from './coin';
 export * from './doors';
-export * from './frame';
+export * from './frames';
 export * from './hall-of-fame';
 export * from './play-room';
 export * from './player';
