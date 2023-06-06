414f0f2841e632ce1d7cc2cf8873af321565126f
Kamil <k.wypych@pap.pl>
2023-06-06 21:17:29 +0200

    feat: connected game with api

--- office-legends/src/components/play-room/components/bandit/bandit.tsx
+++ office-legends/src/components/play-room/components/bandit/bandit.tsx
@@ -1,53 +1,55 @@
 import { App } from 'antd';
+import { api } from 'api';
+import { useCustomMutation } from 'hooks';
 import { useState } from 'react';
 import { useGameStore, useUserStore } from 'store';
 
 import * as Styled from './bandit.styled';
+import { getReward } from './bandit.utils';
 import { BanditBar, Instruction } from './components';
 
 export const Bandit = () => {
   const { message } = App.useApp();
-  const [credits, setCredits] = useState(0);
   const [banditResult, setBanditResult] = useState(new Array<number>(3).fill(1));
   const { isPlayroomOpened } = useGameStore();
-  const { money } = useUserStore();
+  const { money, credits } = useUserStore();
+
+  const duration = 0.5;
+  const okText = credits ? 'Spin!' : 'Try luck!';
+
+  const startGameMutation = useCustomMutation(api.player.updatePlayer, {
+    mutationKey: 'updatePlayer',
+    onSuccess: (success) => {
+      useUserStore.setState({
+        credits: success?.credits,
+        money: success?.money,
+        exp: success?.exp,
+      });
+    },
+  });
+
   const handlePlayroomClose = () => {
     setBanditResult(new Array<number>(3).fill(1));
     useGameStore.setState({ isPlayroomOpened: false });
   };
 
-  const duration = 0.5;
-  const okText = credits ? 'Spin!' : 'Try luck!';
-
   const buyCredits = () => {
-    if (money && money >= 10) return setCredits(5);
+    if (money && money >= 10) return startGameMutation.mutateAsync({ money: money - 10, credits: (credits || 0) + 5 });
 
     return message.error({ content: `You don't have enough money to start game :(`, key: 'playroom-lose' });
   };
 
   const handleSpin = () => {
-    setCredits((prevState) => prevState - 1);
     const results = Array.from({ length: banditResult.length }, () => Math.floor(Math.random() * 3 + 1));
     setBanditResult(results);
 
-    const isWin = results.every((result) => result === results[0]);
+    const reward = getReward(results, message, duration);
 
-    if (isWin) {
-      switch (results[0]) {
-        case 2:
-          return setTimeout(() => message.success('You won 15$'), duration * 1000);
-        case 1:
-          return setTimeout(() => message.success('You won 30$'), duration * 1000);
-        default:
-          return setTimeout(() => message.success('You won 50$'), duration * 1000);
-      }
-    }
-
-    return setTimeout(() => message.error({ content: 'You lose, try again!', key: 'playroom-lose' }), duration * 1000);
+    if (credits && money) startGameMutation.mutateAsync({ credits: credits - 1, money: money + reward });
   };
 
   const okButtonHandler = () => {
-    if (credits > 0) return handleSpin();
+    if (credits && credits > 0) return handleSpin();
 
     return buyCredits();
   };
@@ -62,7 +64,7 @@ export const Bandit = () => {
       okText={okText}
       closeIcon='X'
     >
-      <BanditBar banditResult={banditResult} credits={credits} duration={duration} />
+      <BanditBar banditResult={banditResult} credits={credits || 0} duration={duration} />
       <Instruction />
     </Styled.Wrapper>
   ) : null;
---
+++ office-legends/src/components/play-room/components/bandit/bandit.utils.ts
@@ -0,0 +1,26 @@
+import { MessageInstance } from 'antd/es/message/interface';
+
+export const getReward = (results: number[], message: MessageInstance, duration: number) => {
+  const isWin = results.every((result) => result === results[0]);
+
+  if (isWin) {
+    switch (results[0]) {
+      case 2: {
+        setTimeout(() => message.success('You won 15$'), duration * 1000);
+        return 15;
+      }
+
+      case 1: {
+        setTimeout(() => message.success('You won 30$'), duration * 1000);
+        return 30;
+      }
+      default: {
+        setTimeout(() => message.success('You won 50$'), duration * 1000);
+        return 50;
+      }
+    }
+  } else {
+    setTimeout(() => message.error({ content: 'You lose, try again!', key: 'playroom-lose' }), duration * 1000);
+  }
+  return 0;
+};
